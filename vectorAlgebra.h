28767dfda522dc7a639c273806620a068f81aaaf
Gustavo G <chudijunk@gmail.com>
2010-12-02 18:37:54 +0100

    prototipe added

--- vectorAlgebra.h
+++ vectorAlgebra.h
@@ -3,5 +3,6 @@ Vector vectoresResta(Vector *vec1, Vector *vec2);
 double vectoresDist(Vector *vec1, Vector *vec2);
 double vectorModulo(Vector *vec);
 
+void vectorInitToZero(Vector *x);
 
 double vectorCoorModulo(double *x1, double *y1, double *z1);
