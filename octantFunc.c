3c7e064ef856e892c9ac2e8edd02ff7fcbbf9a2a
Gustavo G <chudijunk@gmail.com>
2010-12-12 18:25:05 +0100

    lala

--- octantFunc.c
+++ octantFunc.c
@@ -49,11 +49,7 @@ Octant getRootOctant(Body bodies[], int qBodies){
     if(c.zStart < 0){ c.zStart = c.zStart*1.1;}else{ c.zStart = c.zStart*0.9;}
 
     /*Correct way of linking arrays? !!!*/
-
-    for(i = 0; i < qBodies; i++){
-        c.bodies[i] = bodies[i];
-    }
-
+    memcpy(&(c.bodies[0]),&(bodies[0]),qBodies);
     printf("qBodies : %d , body 0: %g , 1: %g\n",qBodies, c.bodies[0].masa, c.bodies[10].masa); 
     return c;
 }
